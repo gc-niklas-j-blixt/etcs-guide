fcc5527e9f25c27fa754cc7aff8135ad87de77c4
gc-niklas-j-blixt <niklas.blixt2@greencargo.com>
2026-08-13 12:32:10 +0200

    Split DMI startup nodes into separate guide file

---
+++ data/dmi_startup.js
@@ -0,0 +1,41 @@
+// ==========================
+// DMI-UPPSTART
+// ==========================
+
+Object.assign(guide, {
+
+    // ======== DMI UPPSTART ========
+
+    dmi_boot_check: {
+        title: "Startar DMI?",
+        text: "Observera DMI-skärmen vid uppstart.",
+        choices: [
+            ["Ja", "dmi_display_check"],
+            ["Nej, DMI är svart", "dmi_cb_check"]
+        ]
+    },
+
+    dmi_cb_check: {
+        title: "Har du kontrollerat dvärgbrytarna för DMI?",
+        text: "Kontrollera att dvärgbrytarna för båda lokets DMI:er är i läge 'TILL'.",
+        choices: [
+            ["Ja, men DMI är fortfarande svart", "dmi_boot_failure"],
+            ["Nej, jag har inte kontrollerat dvärgbrytarna", "dmi_cb_reset"],
+        ]
+    },
+
+    dmi_cb_reset: {
+        title: "Kontrollera att dvärgbrytarna är i rätt läge",
+        text: "Sätt dvärgbrytarna i rätt läge och prova igen.",
+        choices: [
+            ["Dvärgbrytarna var i rätt läge, men DMI är fortfarande svart", "dmi_boot_failure"],
+            ["Dvärgbrytarna var i fel läge, nu är de återställda", "dmi_boot_check"],
+        ]
+    },
+
+    dmi_boot_failure: {
+        title: "Felanmäl: DMI startar inte",
+        text: "Felanmäl enligt gällande rutin.",
+        choices: []
+    }
+});
