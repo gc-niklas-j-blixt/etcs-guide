4f6e4596b48f55f604b519a8b2c4f16082946811
gc-niklas-j-blixt <niklas.blixt2@greencargo.com>
2026-09-01 14:28:24 +0200

    Improve search guidance for ETCS startup

--- data/startup.js
+++ data/startup.js
@@ -25,6 +25,18 @@ Object.assign(guide, {
 },
     atp_restart: {
         title: "Har du provat start om systemet?",
+        keywords: [
+  "ETCS startar inte trots ström",
+  "ATP startar inte trots ström",
+  "huvudströmställare är till",
+  "huvudströmställare i rätt läge",
+  "ETCS huvudströmställare till",
+  "starta om ETCS",
+  "starta om ATP",
+  "ETCS omstart",
+  "ATP omstart",
+  "omstart ETCS"
+],
         text: "Prova att starta om systemet genom att göra följande\n\n1).Vrid huvudströmställaren till läge FRÅN/OFF.\n\n2).Vänta minst 10 sekunder innan du slår på systemet igen.\n\n3).Vrid huvudströmställaren till läge TILL/ON\n\n4).Gå tillbaka till hytten och kontrollera DMI.(Det tar 120 sekunder för systemet att start upp).",
         choices: [
             ["Jag har redan provat starta utan framgång", "atp_boot_failure"],
